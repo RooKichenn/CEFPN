9040a1c315475a243d5fba8c8dbda8baf10d9dc0
RooKichenn <78341437+RooKichenn@users.noreply.github.com>
2022-04-18 19:35:21 +0800

    Add files via upload

---
+++ my_dataset.py
@@ -0,0 +1,179 @@
+import os
+import json
+
+import torch
+from PIL import Image
+import torch.utils.data as data
+from pycocotools.coco import COCO
+
+
+def _coco_remove_images_without_annotations(dataset, ids):
+    """
+    删除coco数据集中没有目标，或者目标面积非常小的数据
+    refer to:
+    https://github.com/pytorch/vision/blob/master/references/detection/coco_utils.py
+    :param dataset:
+    :param cat_list:
+    :return:
+    """
+    def _has_only_empty_bbox(anno):
+        return all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno)
+
+    def _has_valid_annotation(anno):
+        # if it's empty, there is no annotation
+        if len(anno) == 0:
+            return False
+        # if all boxes have close to zero area, there is no annotation
+        if _has_only_empty_bbox(anno):
+            return False
+
+        return True
+
+    valid_ids = []
+    for ds_idx, img_id in enumerate(ids):
+        ann_ids = dataset.getAnnIds(imgIds=img_id, iscrowd=None)
+        anno = dataset.loadAnns(ann_ids)
+
+        if _has_valid_annotation(anno):
+            valid_ids.append(img_id)
+
+    return valid_ids
+
+
+class CocoDetection(data.Dataset):
+    """`MS Coco Detection <https://cocodataset.org/>`_ Dataset.
+    Args:
+        root (string): Root directory where images are downloaded to.
+        annFile (string): Path to json annotation file.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(self, root, dataset="train", transforms=None):
+        super(CocoDetection, self).__init__()
+        assert dataset in ["train", "val"], 'dataset must be in ["train", "val"]'
+        anno_file = "instances_{}2017.json".format(dataset)
+        assert os.path.exists(root), "file '{}' does not exist.".format(root)
+        self.img_root = os.path.join(root, "{}2017".format(dataset))
+        assert os.path.exists(self.img_root), "path '{}' does not exist.".format(self.img_root)
+        self.anno_path = os.path.join(root, "annotations", anno_file)
+        assert os.path.exists(self.anno_path), "file '{}' does not exist.".format(self.anno_path)
+
+        self.mode = dataset
+        self.transforms = transforms
+        self.coco = COCO(self.anno_path)
+
+        # 获取coco数据索引与类别名称的关系
+        # 注意在object80中的索引并不是连续的，虽然只有80个类别，但索引还是按照stuff91来排序的
+        data_classes = dict([(v["id"], v["name"]) for k, v in self.coco.cats.items()])
+        max_index = max(data_classes.keys())  # 90
+        # 将缺失的类别名称设置成N/A
+        coco_classes = {}
+        for k in range(1, max_index + 1):
+            if k in data_classes:
+                coco_classes[k] = data_classes[k]
+            else:
+                coco_classes[k] = "N/A"
+
+        if dataset == "train":
+            json_str = json.dumps(coco_classes, indent=4)
+            with open("coco91_indices.json", "w") as f:
+                f.write(json_str)
+
+        self.coco_classes = coco_classes
+
+        ids = list(sorted(self.coco.imgs.keys()))
+        if dataset == "train":
+            # 移除没有目标，或者目标面积非常小的数据
+            valid_ids = _coco_remove_images_without_annotations(self.coco, ids)
+            self.ids = valid_ids
+        else:
+            self.ids = ids
+
+    def parse_targets(self,
+                      img_id: int,
+                      coco_targets: list,
+                      w: int = None,
+                      h: int = None):
+        assert w > 0
+        assert h > 0
+
+        # 只筛选出单个对象的情况
+        anno = [obj for obj in coco_targets if obj['iscrowd'] == 0]
+
+        boxes = [obj["bbox"] for obj in anno]
+
+        # guard against no boxes via resizing
+        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
+        # [xmin, ymin, w, h] -> [xmin, ymin, xmax, ymax]
+        boxes[:, 2:] += boxes[:, :2]
+        boxes[:, 0::2].clamp_(min=0, max=w)
+        boxes[:, 1::2].clamp_(min=0, max=h)
+
+        classes = [obj["category_id"] for obj in anno]
+        classes = torch.tensor(classes, dtype=torch.int64)
+
+        area = torch.tensor([obj["area"] for obj in anno])
+        iscrowd = torch.tensor([obj["iscrowd"] for obj in anno])
+
+        # 筛选出合法的目标，即x_max>x_min且y_max>y_min
+        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
+        boxes = boxes[keep]
+        classes = classes[keep]
+        area = area[keep]
+        iscrowd = iscrowd[keep]
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = classes
+        target["image_id"] = torch.tensor([img_id])
+
+        # for conversion to coco api
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        return target
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
+        """
+        coco = self.coco
+        img_id = self.ids[index]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        coco_target = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(img_id)[0]['file_name']
+        img = Image.open(os.path.join(self.img_root, path)).convert('RGB')
+
+        w, h = img.size
+        target = self.parse_targets(img_id, coco_target, w, h)
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.ids)
+
+    def get_height_and_width(self, index):
+        coco = self.coco
+        img_id = self.ids[index]
+
+        img_info = coco.loadImgs(img_id)[0]
+        w = img_info["width"]
+        h = img_info["height"]
+        return h, w
+
+    @staticmethod
+    def collate_fn(batch):
+        return tuple(zip(*batch))
+
+
+# train = CocoDetection("/data/coco_data/", dataset="train")
+# print(len(train))
+# t = train[0]
+# print(t)
---
+++ validation.py
@@ -0,0 +1,201 @@
+"""
+该脚本用于调用训练好的模型权重去计算验证集/测试集的COCO指标
+以及每个类别的mAP(IoU=0.5)
+"""
+
+import os
+import json
+
+import torch
+import torchvision
+from tqdm import tqdm
+import numpy as np
+from torchvision.models.feature_extraction import create_feature_extractor
+
+import transforms
+from network_files import FasterRCNN, AnchorsGenerator
+from my_dataset import CocoDetection
+from backbone import resnet50_fpn_backbone
+from train_utils import EvalCOCOMetric
+
+
+def summarize(self, catId=None):
+    """
+    Compute and display summary metrics for evaluation results.
+    Note this functin can *only* be applied on the default parameter setting
+    """
+
+    def _summarize(ap=1, iouThr=None, areaRng='all', maxDets=100):
+        p = self.params
+        iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
+        titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+        typeStr = '(AP)' if ap == 1 else '(AR)'
+        iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+            if iouThr is None else '{:0.2f}'.format(iouThr)
+
+        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+
+        if ap == 1:
+            # dimension of precision: [TxRxKxAxM]
+            s = self.eval['precision']
+            # IoU
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+
+            if isinstance(catId, int):
+                s = s[:, :, catId, aind, mind]
+            else:
+                s = s[:, :, :, aind, mind]
+
+        else:
+            # dimension of recall: [TxKxAxM]
+            s = self.eval['recall']
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+
+            if isinstance(catId, int):
+                s = s[:, catId, aind, mind]
+            else:
+                s = s[:, :, aind, mind]
+
+        if len(s[s > -1]) == 0:
+            mean_s = -1
+        else:
+            mean_s = np.mean(s[s > -1])
+
+        print_string = iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s)
+        return mean_s, print_string
+
+    stats, print_list = [0] * 12, [""] * 12
+    stats[0], print_list[0] = _summarize(1)
+    stats[1], print_list[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
+    stats[2], print_list[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
+    stats[3], print_list[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
+    stats[4], print_list[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
+    stats[5], print_list[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
+    stats[6], print_list[6] = _summarize(0, maxDets=self.params.maxDets[0])
+    stats[7], print_list[7] = _summarize(0, maxDets=self.params.maxDets[1])
+    stats[8], print_list[8] = _summarize(0, maxDets=self.params.maxDets[2])
+    stats[9], print_list[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
+    stats[10], print_list[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
+    stats[11], print_list[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+
+    print_info = "\n".join(print_list)
+
+    if not self.eval:
+        raise Exception('Please run accumulate() first')
+
+    return stats, print_info
+
+
+def main(parser_data):
+    device = torch.device(parser_data.device if torch.cuda.is_available() else "cpu")
+    print("Using {} device training.".format(device.type))
+
+    data_transform = {
+        "val": transforms.Compose([transforms.ToTensor()])
+    }
+
+    # read class_indict
+    label_json_path = './coco91_indices.json'
+    assert os.path.exists(label_json_path), "json file {} dose not exist.".format(label_json_path)
+    with open(label_json_path, 'r') as f:
+        category_index = json.load(f)
+
+    coco_root = parser_data.data_path
+
+    # 注意这里的collate_fn是自定义的，因为读取的数据包括image和targets，不能直接使用默认的方法合成batch
+    batch_size = parser_data.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using %g dataloader workers' % nw)
+
+    # load validation data set
+    val_dataset = CocoDetection(coco_root, "val", data_transform["val"])
+    val_dataset_loader = torch.utils.data.DataLoader(val_dataset,
+                                                     batch_size=batch_size,
+                                                     shuffle=False,
+                                                     pin_memory=True,
+                                                     num_workers=nw,
+                                                     collate_fn=val_dataset.collate_fn)
+
+    # create model
+    backbone = resnet50_fpn_backbone(norm_layer=torch.nn.BatchNorm2d)
+    model = FasterRCNN(backbone=backbone, num_classes=parser_data.num_classes + 1)
+
+    # 载入你自己训练好的模型权重
+    weights_path = parser_data.weights
+    assert os.path.exists(weights_path), "not found {} file.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path, map_location='cpu')['model'])
+    # print(model)
+
+    model.to(device)
+
+    # evaluate on the val dataset
+    cpu_device = torch.device("cpu")
+
+    det_metric = EvalCOCOMetric(val_dataset.coco, "bbox", "det_results.json")
+    model.eval()
+    with torch.no_grad():
+        for image, targets in tqdm(val_dataset_loader, desc="validation..."):
+            # 将图片传入指定设备device
+            image = list(img.to(device) for img in image)
+
+            # inference
+            outputs = model(image)
+            outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+            det_metric.update(targets, outputs)
+
+    det_metric.synchronize_results()
+    det_metric.evaluate()
+
+    # calculate COCO info for all classes
+    coco_stats, print_coco = summarize(det_metric.coco_evaluator)
+
+    # calculate voc info for every classes(IoU=0.5)
+    voc_map_info_list = []
+    classes = [v for v in category_index.values() if v != "N/A"]
+    for i in range(len(classes)):
+        stats, _ = summarize(det_metric.coco_evaluator, catId=i)
+        voc_map_info_list.append(" {:15}: {}".format(classes[i], stats[1]))
+
+    print_voc = "\n".join(voc_map_info_list)
+    print(print_voc)
+
+    # 将验证结果保存至txt文件中
+    with open("record_mAP.txt", "w") as f:
+        record_lines = ["COCO results:",
+                        print_coco,
+                        "",
+                        "mAP(IoU=0.5) for each category:",
+                        print_voc]
+        f.write("\n".join(record_lines))
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+
+    # 使用设备类型
+    parser.add_argument('--device', default='cuda', help='device')
+
+    # 检测目标类别数
+    parser.add_argument('--num-classes', type=int, default=90, help='number of classes')
+
+    # 数据集的根目录(coco2017根目录)
+    parser.add_argument('--data-path', default='./coco2017', help='dataset root')
+
+    # 训练好的权重文件
+    parser.add_argument('--weights', default='./multi_train/model_25.pth', type=str, help='training weights')
+
+    # batch size
+    parser.add_argument('--batch_size', default=1, type=int, metavar='N',
+                        help='batch size when validation.')
+
+    args = parser.parse_args()
+
+    main(args)
